9e8207e97b85cb73ead7379e4d5b7c158c4213e2
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-03-16 14:21:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 
 module.exports = fibonacci;
 */
-
+/*
 function fibonacci(num){
 	if(num === 0 || num === 1){
 		return 0 ;
@@ -25,4 +25,18 @@ function fibonacci(num){
 }
 
 
+module.exports = fibonacci ;*/
+function fibonacci(num) {
+let a = 0, b = 1, temp;
+
+if (num === 1 ||num==0) {
+    return 0;
+}
+for (let i = 2; i <= num; i++) {
+    temp = a + b;
+    a = b;
+    b = temp;
+}
+return b;
+}
 module.exports = fibonacci ;
